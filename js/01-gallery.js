cc8873fede008de9e66cacae1bfbba8d81c0dfb0
Vitaliy <vitaliy.bratash23@gmail.com>
2022-08-31 19:10:43 +0300

    JS-Homework

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+import * as basicLightbox from "../node_modules/basiclightbox/src/scripts/main.js";
+
+const gallery = document.querySelector(`.gallery`);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute("data-source")}" width="800" height="600">`);
+    instance.show();
+  } 
+});
+
+function createGallery(images) {
+  return images
+    .map((item) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"/>
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+
+gallery.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(`.gallery`);
+
+function createGallery(images) {
+  return images
+    .map((item) => {
+      return `
+    <a class="gallery__item" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"/>
+    </a>`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+console.log(galleryItems);
+
+ new SimpleLightbox ('.gallery a',{ captionsData: 'alt', captionDelay: 250 });
